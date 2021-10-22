4b063e68df150383df970c5c8a3f7a5d2c1b6a9c
Apen <cerdanyohann@yahoo.fr>
2021-10-22 17:15:36 +0200

    [TASK] TER version 1.6.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,22 +4,18 @@ $EM_CONF['recordsmanager'] = [
     'title' => 'Records management in a BE module',
     'description' => 'Add modules to easily manage your records (insert, edit & export in be/eId) in one place.',
     'category' => 'module',
-    'version' => '1.6.0-dev',
+    'version' => '1.6.0',
     'state' => 'stable',
     'createDirs' => '',
     'clearcacheonload' => true,
     'author' => 'CERDAN Yohann [Site-nGo]',
     'author_email' => 'cerdanyohann@yahoo.fr',
     'author_company' => '',
-    'constraints' =>
-        [
-            'depends' =>
-                [
-                    'typo3' => '10.4.0-11.5.99',
-                ],
-            'conflicts' =>
-                [],
-            'suggests' =>
-                [],
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-11.5.99',
         ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
 ];
