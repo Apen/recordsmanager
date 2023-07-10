f6c1c96e72c29f03da7372afbc00277958d9d58f
Apen <cerdanyohann@gmail.com>
2023-07-10 15:47:57 +0200

    [TASK] Clean emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,36 +1,32 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "recordsmanager".
- *
- * Auto generated 16-11-2021 15:06
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['recordsmanager'] = [
     'title' => 'Records management in a BE module',
     'description' => 'Add modules to easily manage your records (insert, edit & export in be/eId) in one place.',
     'category' => 'module',
     'version' => '1.6.7',
     'state' => 'stable',
-    'clearCacheOnLoad' => true,
+    'uploadfolder' => false,
+    'clearcacheonload' => false,
     'author' => 'CERDAN Yohann [Site-nGo]',
     'author_email' => 'cerdanyohann@yahoo.fr',
     'author_company' => '',
-    'constraints' =>
-    [
+    'constraints' => [
         'depends' =>
-        [
-            'typo3' => '10.4.0-11.5.99',
-        ],
+            [
+                'typo3' => '10.4.0-11.5.99',
+            ],
         'conflicts' =>
-        [
-        ],
+            [
+            ],
         'suggests' =>
-        [
+            [
+            ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Sng\\Recordsmanager\\' => 'Classes/',
         ],
     ],
 ];
+
