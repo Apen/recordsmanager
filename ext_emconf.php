7c7e6771f440a936b0888e164fd3962aeb36112a
Apen <cerdanyohann@gmail.com>
2025-06-13 14:19:32 +0200

    [TASK] Extend PHP and TYPO3 version requirements in composer.json and ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF['recordsmanager'] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '12.4.99-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
